d70de7b72d6eca5e5271e65bc861f729c60b08e7
Morad elm <78969860+MoradElm96@users.noreply.github.com>
2023-03-17 17:35:29 +0100

    primer commit sin backend

--- js/scriptphp.js
+++
@@ -1,9 +0,0 @@
-function descargarPDF() {
-    // Obtener el nombre del archivo PDF que se va a descargar
-    var fileName = "CvMorad.pdf";
-    
-    // Crear una URL que apunta al script PHP que descarga el archivo
-    var url = "php/cv.php?file=" + fileName;
-    
-    // Redirigir a la URL creada para descargar el archivo
-    window.location.href = url;}
